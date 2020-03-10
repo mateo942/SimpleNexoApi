1a50290ae9fdc1ed4f040ada1e8c1869703a28a0
Mateusz <m.p@pro.com>
2020-03-10 17:51:14 +0100

    Add project files.

---
+++ Nexo/Controllers/WarehouseController.cs
@@ -0,0 +1,22 @@
+﻿using InsERT.Moria.ModelOrganizacyjny;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Nexo.Controllers
+{
+    public class WarehouseController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            var warehouseRepository = WebApiApplication.Uchwyt.PodajObiektTypu<IMagazyny>();
+
+            var results = warehouseRepository.Dane.Wszystkie().Select(x => x.Id);
+
+            return Ok(results);
+        }
+    }
+}
---
+++ Nexo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nexo.WebApiApplication" Language="C#" %>
